e1a30dffbdf0c07260ca31d6c8479fe72bc08edf
Dwima Basumatary <dwima1583@gmail.com>
2023-05-29 20:12:03 +0530

    add Seeds

---
+++ src/seeds/expenseSeeds.js
@@ -0,0 +1,96 @@
+const EXPENSE_SEEDS = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2022, 2, 15), // March 15, 2022
+    },
+    { 
+      id: 'e2', 
+      title: 'New TV', 
+      amount: 799.49, 
+      date: new Date(2023, 6, 28), // July 28, 2023
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2022, 11, 1), // December 1, 2022
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 0, 5), // January 5, 2023
+    },
+    {
+      id: 'e5',
+      title: 'Groceries',
+      amount: 75.49,
+      date: new Date(2022, 2, 15), // March 15, 2022
+    },
+    { 
+      id: 'e6', 
+      title: 'Restaurant Dinner', 
+      amount: 120.99, 
+      date: new Date(2023, 6, 28), // July 28, 2023
+    },
+    {
+      id: 'e7',
+      title: 'Movie Tickets',
+      amount: 32.5,
+      date: new Date(2020, 11, 1), // December 1, 2022
+    },
+    {
+      id: 'e8',
+      title: 'Fitness Membership',
+      amount: 99.99,
+      date: new Date(2020, 0, 5), // January 5, 2023
+    },
+    {
+      id: 'e9',
+      title: 'Vacation Expenses',
+      amount: 1500,
+      date: new Date(2021, 7, 10), // August 10, 2022
+    },
+    { 
+      id: 'e10', 
+      title: 'Electronics Purchase', 
+      amount: 899.99, 
+      date: new Date(2023, 2, 5), // March 5, 2023
+    },
+    {
+      id: 'e11',
+      title: 'Home Renovation',
+      amount: 2500,
+      date: new Date(2019, 4, 20), // May 20, 2022
+    },
+    {
+      id: 'e12',
+      title: 'Birthday Celebration',
+      amount: 300,
+      date: new Date(2023, 8, 12), // September 12, 2023
+    },
+    {
+      id: 'e13',
+      title: 'Monthly Utilities',
+      amount: 200,
+      date: new Date(2022, 9, 1), // October 1, 2022
+    },
+    {
+      id: 'e14',
+      title: 'Gym Membership',
+      amount: 50,
+      date: new Date(2021, 6, 15), // July 15, 2021
+    },
+    { 
+      id: 'e15', 
+      title: 'Holiday Gifts', 
+      amount: 500, 
+      date: new Date(2019, 11, 24), // December 24, 2019
+    },
+  ];
+  
+  
+
+  export default EXPENSE_SEEDS;
